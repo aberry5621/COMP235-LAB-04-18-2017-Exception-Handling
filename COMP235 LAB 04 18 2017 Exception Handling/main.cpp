a9142e6e99d6547bc2b3ad85c12f7ca8f2863e93
aberry5621 <aberry5621@gmail.com>
2017-04-28 08:47:15 -0700

    Initial Commit

---
+++ COMP235 LAB 04 18 2017 Exception Handling/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  COMP235 LAB 04 18 2017 Exception Handling
+//
+//  Created by ax on 4/28/17.
+//  Copyright © 2017 COMP235. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
